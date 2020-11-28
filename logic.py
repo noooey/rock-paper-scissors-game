97300885e7c05fbd44c1c9f4f31bec0c4dba205a
kueyeon0429 <20203065@kookmin.ac.kr>
2020-11-29 05:33:17 +0900

    modify method

--- logic.py
+++ logic.py
@@ -5,16 +5,16 @@ class gameLogic:
      #  잔액 100원 차감, 가위 바위 보 랜덤 값 리턴
     def setGame(self, bal):
         bal -= 100
-        computer_rps = random.random(1, 4)  # 1: 가위  2: 바위  3: 보
+        computer_rps = random.randint(1, 3)  # 1: 가위  2: 바위  3: 보
         return computer_rps
 
 
      #  게임 메인 로직 구현
-    def mainLogic(self, rps, computer_rps, bal):
+    def mainLogic(self, myRps, com_rps, bal):
 
-        if rps != computer_rps:
+        if myRps != com_rps:
              #  승
-            if (rps == 1 and computer_rps == 3) or (rps == 2 and computer_rps == 1) or (rps == 3 and computer_rps == 2):
+            if (myRps == 1 and com_rps == 3) or (myRps == 2 and com_rps == 1) or (myRps == 3 and com_rps == 2):
                 bal += self.randomPrice
                 result = "win"
              #  패
@@ -29,4 +29,4 @@ class gameLogic:
 
      #  100~500원 랜덤 금액 설정
     def randomPrice(self):
-        return random.radom(1, 6)*100
+        return random.randint(1, 5)*100
