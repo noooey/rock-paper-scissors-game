0e988b2b03549aa8ca71d42f92f0bfbf364c8168
kueyeon0429 <20203065@kookmin.ac.kr>
2020-12-01 00:54:25 +0900

    jonna modify

--- logic.py
+++ logic.py
@@ -1,32 +1,36 @@
 import random
+from balance import Balance
 
-class gameLogic:
+class Game:
 
-     #  잔액 100원 차감, 가위 바위 보 랜덤 값 리턴
-    def setGame(self, bal):
-        bal -= 100
-        computer_rps = random.randint(1, 3)  # 1: 가위  2: 바위  3: 보
-        return computer_rps
+      # 가위바위보 랜덤 값 리턴
+    def __init__(self):
+        self.computer_rps = random.randint(1, 3)  # 1: 가위  2: 바위  3: 보
 
+    def getComputerRps(self):
+        return self.computer_rps
 
-     #  게임 메인 로직 구현
-    def mainLogic(self, myRps, com_rps, bal):
-
-        if myRps != com_rps:
-             #  승
-            if (myRps == 1 and com_rps == 3) or (myRps == 2 and com_rps == 1) or (myRps == 3 and com_rps == 2):
-                bal += self.randomPrice
+        #  승패무 결정
+    def determineWinOrLose(self, myRps):
+        if myRps != self.getComputerRps:
+            #  승
+            if (myRps == 1 and self.getComputerRps == 3) or (myRps == 2 and self.getComputerRps == 1) or (myRps == 3 and self.getComputerRps == 2):
                 result = "win"
-             #  패
+            #  패
             else:
-                 bal += 0
-                 result = "lose"
-         #  무
+                result = "lose"
+        #  무
         else:
-            bal += 100
             result = "draw"
         return result
 
-     #  100~500원 랜덤 금액 설정
-    def randomPrice(self):
-        return random.randint(1, 5)*100
+        # 금액 추가
+    def addPrice(self, result):
+        self.balance = Balance()
+        if result == "win":
+            self.balance.winPrice
+        elif result == "lose":
+            self.balance.losePrice
+        elif result == "draw":
+            self.balance.drawPrice
+
