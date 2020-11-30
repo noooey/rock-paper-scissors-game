bf390d38e9a2ee70276e3820d4683fa1f48575ef
kueyeon0429 <66217855+kueyeon0429@users.noreply.github.com>
2020-12-01 03:57:25 +0900

    Add files via upload

--- balance.py
+++ balance.py
@@ -5,23 +5,20 @@ class Balance:
     def __init__(self):
         self.balance = 300
 
-    def getBalance(self):
-        return self.balance
-
     def deductPrice(self):
         self.balance -= 100
 
     def winPrice(self):
         self.balance += self.randomPrice()
 
-    def losePrcie(self):
+    def losePrice(self):
         self.balance += 0
 
     def drawPrice(self):
         self.balance += 100
 
     def currentBalance(self):
-        return str(self.balance)
+        return self.balance
 
         #  100~500원 랜덤 금액 설정
     def randomPrice(self):
